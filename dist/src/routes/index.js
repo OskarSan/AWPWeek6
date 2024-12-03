6a1a66b1dcc092c9c3f359d7bb9d55d104795383
OskarSan <oskar.sankila@gmail.com>
2024-12-03 12:12:15 +0200

    task 1

--- dist/src/routes/index.js
+++ dist/src/routes/index.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const image_1 = require("../models/image");
+const Image_1 = require("../models/Image");
 const multer_config_1 = __importDefault(require("../middleware/multer-config"));
+const Offer_1 = require("../models/Offer");
 const router = (0, express_1.Router)();
 router.post("/upload", multer_config_1.default.single('image'), async (req, res) => {
     try {
+        const offer = new Offer_1.Offer({
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        });
+        console.log(offer);
+        await offer.save();
         if (!req.file) {
             res.status(400).json({
                 status: false,
@@ -16,14 +24,16 @@ router.post("/upload", multer_config_1.default.single('image'), async (req, res)
             });
             return;
         }
-        const imgPath = req.file.path.replace("public", "");
-        const image = new image_1.Image({
-            filename: req.file.filename,
-            description: req.body.description,
-            path: imgPath
-        });
-        await image.save();
-        res.json({ message: "File uploaded successfully", path: imgPath });
+        else {
+            const imgPath = req.file.path.replace("public", "");
+            const image = new Image_1.Image({
+                filename: req.file.filename,
+                description: req.body.description,
+                path: imgPath
+            });
+            await image.save();
+            res.json({ message: "Image uploaded successfully", path: imgPath });
+        }
     }
     catch (error) {
         console.log(error);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,31 +2,51 @@ import { Router, Request, Response } from "express";
 import multer from "multer";
 import {Image, IImage } from "../models/Image";
 import upload from "../middleware/multer-config";
+import { Offer, IOffer } from "../models/Offer";
 
 const router: Router = Router();
 
 
 router.post("/upload", upload.single('image'), async (req : Request, res : Response) => {
     try{
+        
+        const offer : IOffer = new Offer({
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        });
+
+        
+        console.log(offer);
+        await offer.save();
+        
         if(!req.file){
             res.status(400).json({
                 status: false,
                 data: "No file is selected."
             });
             return;
+        } else {
+            const imgPath : string = req.file.path.replace("public", "");
+
+            const image : IImage = new Image({
+                filename: req.file.filename,
+                description: req.body.description,
+                path: imgPath
+            });
+            await image.save();
+            res.json({message: "Image uploaded successfully", path: imgPath});
         }
     
-        const imgPath : string = req.file.path.replace("public", "");
+       
+        
+     
+        
+        
 
-        const image : IImage = new Image({
-            filename: req.file.filename,
-            description: req.body.description,
-            path: imgPath
-        });
+        
 
-        await image.save();
 
-        res.json({message: "File uploaded successfully", path: imgPath});
     } catch (error) {
         console.log(error);
     } 
